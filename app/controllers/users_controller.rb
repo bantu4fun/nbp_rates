028e07662c1add4a803f04070b25d4589e43203a
bantu4fun <bantu4fun@gmail.com>
2016-04-13 13:13:55 +0200

    Convert rspec 2 to 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+  before_action :set_user, only: [:show]
+
+  def show
+    
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
 
 end
